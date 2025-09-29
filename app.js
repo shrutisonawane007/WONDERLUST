699c1f94bf757ade5915cdf189707051c59774f1
TG404-JV <160750407+shrutisonawane007@users.noreply.github.com>
2025-09-29 14:39:06 +0530

    added edit page and new page

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const listing = require("./models/listing.js");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
+const { redirect } = require("react-router-dom");
 
 const MONGO_URL= "mongodb://127.0.0.1:27017/wanderlust";
 main().then(()=>{
@@ -19,6 +21,7 @@ async function main() {
 app.set("view engine","ejs");
 app.set("views", path.join(__dirname,"views"));
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 app.get("/",(req,res)=>{
     res.send("Hi, i am root");
@@ -30,6 +33,12 @@ app.get("/listings",async(req,res)=>{
     res.render("listings/index.ejs",{allListings});
 });
 
+//New Route
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs");
+});
+
+
 //show route 
 app.get("/listings/:id", async (req,res)=>{
     let {id} = req.params;
@@ -37,7 +46,26 @@ app.get("/listings/:id", async (req,res)=>{
     res.render("listings/show.ejs",{listing});
 });
 
-//
+//Create Route
+app.post("/listings",async(req,res)=>{
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+
+//edit route
+app.get("/listings/:id/edit",async(req,res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id); 
+    res.render("listings/edit.ejs",{listing});
+});
+
+//upadate route
+app.put("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
+});
 
 // app.get ("/testlisting" ,async (req,res)=>{
 //     let sampleListing= new listing({
