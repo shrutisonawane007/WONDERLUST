a956b71315e12a97edc583406c4360540f6874f9
TG404-JV <160750407+shrutisonawane007@users.noreply.github.com>
2025-10-07 21:58:26 +0530

    added server side valudation for reviews

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-Mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/expressError.js");
-const {listingSchema} =require("./schema.js");
+const {listingSchema,reviewSchema} =require("./schema.js");
 const Review = require("./models/review.js");
+
 
 const MONGO_URL= "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -35,6 +36,16 @@ app.get("/",(req,res)=>{
 
 const validateListing = (req,res,next)=>{
   let {error} = listingSchema.validate(req.body);
+  if(error){
+    let errMsg = error.details.map((el)=>el.message).join(",");
+        throw new ExpressError (400,errMsg);
+    }else{
+        next();
+    }
+};
+
+const validateReview = (req,res,next)=>{
+  let {error} = reviewSchema.validate(req.body);
   if(error){
     let errMsg = error.details.map((el)=>el.message).join(",");
         throw new ExpressError (400,errMsg);
@@ -95,7 +106,7 @@ app.delete("/listings/:id",  wrapAsync( async(req,res)=>{
 
 //reviews
 //post route
-app.post("/listings/:id/reviews",async(req,res)=>{
+app.post("/listings/:id/reviews",validateReview,wrapAsync( async(req,res)=>{
    let listing = await Listing.findById(req.params.id);
    let newReview = new Review(req.body.review);
 
@@ -105,7 +116,7 @@ app.post("/listings/:id/reviews",async(req,res)=>{
    await listing.save();
 
   res.redirect(`/listings/${listing._id}`);
-});
+}));
 
 app.all("*",(req,res,next)=>{
    next(new ExpressError(404,"page not found"));
