155531a8132c47feb8f3ffa11160b969f52ce072
shruti sonawane <160750407+shrutisonawane007@users.noreply.github.com>
2025-12-14 11:19:38 +0530

    added flash

--- routes/listing.js
+++ routes/listing.js
@@ -32,6 +32,10 @@ router.get("/new",(req,res)=>{
 router.get("/:id", wrapAsync(async (req,res)=>{
     let {id} = req.params;
     const listing = await Listing.findById(id).populate("reviews");
+    if(!listing){
+      req.flash("error","Cannot find that listing!");  
+      res.redirect("/listings");
+    }
     res.render("listings/show.ejs",{listing});
 }));
 
@@ -49,6 +53,10 @@ router.post("/",validateListing,
 router.get("/:id/edit", wrapAsync(async(req,res)=>{
     let {id} = req.params;
     const listing = await Listing.findById(id); 
+    if(!listing){
+      req.flash("error","Cannot find that listing!");  
+      res.redirect("/listings");
+    }
     res.render("listings/edit.ejs",{listing});
 }));
 
