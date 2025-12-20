c53dad2ff0ef8da9b495a885fd2cf0b7dccf45a6
shruti sonawane <160750407+shrutisonawane007@users.noreply.github.com>
2025-12-20 17:52:22 +0530

    Update review.js

--- routes/review.js
+++ routes/review.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router({mergeParams : true });
 const wrapAsync = require("../utils/wrapAsync.js");
-const ExpressError = require("../utils/ExpressError.js");
+const ExpressError = require("../utils/expressError.js");
 const {validateReview, isLoggedIn, isRevieweAuthor} = require("../middlewares.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
